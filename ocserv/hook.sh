d603a4340f1bbf772562f0e3f57d89d1e48e167d
Soberia <saberhayati@outlook.com>
2023-07-26 05:46:36 +0330

    Store `OpenConnect` connection script into a file

---
+++ ocserv/hook.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ $ENABLE_IPV6 != "true" ]]; then
+    exit 0
+fi
+
+# Configuring the NDP Proxy for the downstream clients
+ip -6 neighbour $([[ $REASON == "connect" ]] && echo add || echo del) \
+    proxy $IPV6_REMOTE dev eth0
