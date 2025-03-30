61793c971f2804f280ed6caf086bf73b1c9de209
Saber Hayati <SaberHayati@outlook.com>
2025-03-30 06:18:05 +0330

    Convert Bash to Shell script

--- ocserv/hook.sh
+++ ocserv/hook.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if [[ $ENABLE_IPV6 != true ]]; then
-    exit 0
+if [ "$ENABLE_IPV6" = true ]; then
+    # Configuring the NDP Proxy for the downstream clients
+    ip -6 neighbour $([ "$REASON" = "connect" ] && echo add || echo del) \
+        proxy $IPV6_REMOTE dev eth0
+    exit $?
 fi
 
-# Configuring the NDP Proxy for the downstream clients
-ip -6 neighbour $([[ $REASON == "connect" ]] && echo add || echo del) \
-    proxy $IPV6_REMOTE dev eth0
+exit 0
